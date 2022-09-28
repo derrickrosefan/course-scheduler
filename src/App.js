d1a6ac9bcd1fec6bc7f1c2ace967210df6b5148f
derrickrosefan <114096803+derrickrosefan@users.noreply.github.com>
2022-09-28 15:04:37 -0500

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
-import CourseList from './components/courseList/courseList';
-import Banner from './components/banner/banner';
+import Schedule from './components/schedule/schedule';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101": {
-      "term": "Fall",
-      "number": "101",
-      "meets": "MWF 11:00-11:50",
-      "title": "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110": {
-      "term": "Fall",
-      "number": "110",
-      "meets": "MWF 10:00-10:50",
-      "title": "Intro Programming for non-majors"
-    },
-    "S313": {
-      "term": "Spring",
-      "number": "313",
-      "meets": "TuTh 15:30-16:50",
-      "title": "Tangible Interaction Design and Learning"
-    },
-    "S314": {
-      "term": "Spring",
-      "number": "314",
-      "meets": "TuTh 9:30-10:50",
-      "title": "Tech & Human Interaction"
-    }
-  }
-};
+const queryClient = new QueryClient()
 
 const App = () => {
-  return (
-    <div className="container-fluid">
-      <Banner title={schedule.title} />
-      <CourseList courses={schedule.courses} />
-    </div>
-  );
+  return <QueryClientProvider client={queryClient}><Schedule /></QueryClientProvider>;
 }
 
 export default App;
