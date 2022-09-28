16f2eb2403aa21a5676e318626538df659be62b0
derrickrosefan <114096803+derrickrosefan@users.noreply.github.com>
2022-09-28 16:15:36 -0500

    changed spaces to tabs

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 const App = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Schedule />
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Schedule />
+		</QueryClientProvider>
+	);
 };
 
 export default App;
