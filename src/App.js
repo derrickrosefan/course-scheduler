92c30407e8955512874b3d9c73575a355338734c
derrickrosefan <114096803+derrickrosefan@users.noreply.github.com>
2022-09-28 15:07:01 -0500

    ran linter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Schedule from './components/schedule/schedule';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Schedule from "./components/schedule/schedule";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <QueryClientProvider client={queryClient}><Schedule /></QueryClientProvider>;
-}
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Schedule />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
