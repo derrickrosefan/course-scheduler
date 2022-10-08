5ad844ba03410f13bc2450216d9bb91cb9872888
derrickrosefan <114096803+derrickrosefan@users.noreply.github.com>
2022-10-07 23:15:59 -0500

    add form validation

--- src/components/courseForm/courseForm.js
+++ src/components/courseForm/courseForm.js
@@ -1,21 +1,37 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
-const InputField = ({ name, text, state, change }) => (
-    <div className="mb-3">
-        <label htmlFor={name} className="form-label">{text}</label>
-        <input className="form-control" id={name} name={name}
-            defaultValue={state.values?.[name]} onChange={change} />
-    </div>
-);
+const InputField = ({ name, text, state, change }) => {
+    return (
+        <div className="mb-3">
+            <label htmlFor={name} className="form-label">{text}</label>
+            <input className="form-control" id={name} name={name}
+                defaultValue={state.values?.[name]} onChange={change} />
+            <div className="text-danger mt-3"><p>{state.errors?.[name]}</p></div>
+        </div>
+    )
+}
 
-export const useFormData = (values = {}) => {
+const validateUserData = (key, val) => {
+    switch (key) {
+        case 'title':
+            return /\w{2,}/gm.test(val) ? '' : 'Invalid title - needs to be at least two characters';
+        case 'meets':
+            return /[M|Tu|W|Th|F]+ [0-9][0-9]:[0-9][0-9]-[0-9][0-9]:[0-9][0-9]/gm.test(val) ? '' : 'Invalid meeting times - must specify valid days (MTuWThF) followed by a space and valid military-style times';
+        default: return '';
+    }
+};
+
+export const useFormData = (validator = null, values = {}) => {
     const [state, setState] = useState(() => ({ values }));
-
     const change = (evt) => {
         const { id, value } = evt.target;
+        const error = validator ? validator(id, value) : '';
+        evt.target.setCustomValidity(error);
         const values = { ...state.values, [id]: value };
-        setState({ values });
+        const errors = { ...state.errors, [id]: error };
+        const hasError = Object.values(errors).some(x => x !== '');
+        setState(hasError ? { values, errors } : { values });
     };
 
     return [state, change];
@@ -32,7 +48,7 @@ const ButtonBar = () => {
 
 const CourseForm = ({ data }) => {
     const { id } = useParams();
-    const [state, change] = useFormData(data.courses[id]);
+    const [state, change] = useFormData(validateUserData, data.courses[id]);
     return (
         <div className="container pt-3">
             <h2>Edit Course Information</h2>
